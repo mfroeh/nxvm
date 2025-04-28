b8cac47102cb6ab2653621eaf784ac822a45d702
mfroeh <mfroeh0@pm.me>
2025-04-28 13:49:00 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixvim.url = "github:nix-community/nixvim";
+    nixvim.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    { nixpkgs, nixvim, ... }@inputs:
+    let
+      systems = [ "aarch64-darwin" "x86_64-linux" ];
+    in
+    {
+      packages = nixpkgs.lib.genAttrs systems (system: {
+        default = nixvim.legacyPackages.${system}.makeNixvim {
+          colorschemes.gruvbox.enable = true;
+        };
+      });
+    };
+}
