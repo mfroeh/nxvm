8786ed24fec4d2d244da6eecf61c7d7173a65a40
mfroeh <mfroeh0@pm.me>
2025-05-07 22:26:18 +0200

    add arguments moving

--- config/treesitter.nix
+++ config/treesitter.nix
@@ -1,40 +1,43 @@
-{ ... }: {
-	plugins.treesitter = {
-		enable = true;
-		settings = {
-			highlight.enable = true;
-			indent.enable = true;
-		};
-	};
+{ ... }:
+{
+  plugins.treesitter = {
+    enable = true;
+    settings = {
+      highlight.enable = true;
+      indent.enable = true;
+    };
+  };
 
-	plugins.treesitter-context.enable = true;
+  plugins.treesitter-context.enable = true;
 
-	plugins.treesitter-textobjects = {
-		enable = true;
-		select = {
-			enable = true;
-			lookahead = true;
-			includeSurroundingWhitespace = true;
-			keymaps = {
-				"aa" = "@parameter.outer";
-				"ia" = "@parameter.inner";
-				"af" = "@function.outer";
-				"if" = "@function.inner";
-				"ac" = "@class.outer";
-				"ic" = "@class.inner";
-			};
-		};
-		move = {
-			enable = true;
-			setJumps = true;
-			gotoNextStart = {
-				"]f" = "@function.outer";
-				"]]" = "@class.outer";
-			};
-			gotoPreviousStart = {
-				"[f" = "@function.outer";
-				"[[" = "@class.outer";
-			};
-		};
-	};
+  plugins.treesitter-textobjects = {
+    enable = true;
+    select = {
+      enable = true;
+      lookahead = true;
+      includeSurroundingWhitespace = true;
+      keymaps = {
+        "aa" = "@parameter.outer";
+        "ia" = "@parameter.inner";
+        "af" = "@function.outer";
+        "if" = "@function.inner";
+        "ac" = "@class.outer";
+        "ic" = "@class.inner";
+      };
+    };
+    move = {
+      enable = true;
+      setJumps = true;
+      gotoPreviousStart = {
+        "[f" = "@function.outer";
+        "[[" = "@class.outer";
+        "[a" = "parameter.outer";
+      };
+      gotoNextStart = {
+        "]f" = "@function.outer";
+        "]]" = "@class.outer";
+        "]a" = "parameter.outer";
+      };
+    };
+  };
 }
